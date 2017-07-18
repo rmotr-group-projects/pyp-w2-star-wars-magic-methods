101c03770213f23d91e094efb9046b5b495db035
Marouane Ab <marouane.abra@gmail.com>
2017-07-18 20:16:15 +0000

    added init function

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,7 @@
 class People(object):
-    def __init__(self, name, dark_side=False):
-        pass
+    def __init__(self, name, dark_side=False,light_side=True):
+        self.name=name
+        self.dark_side=dark_side
+        self.light_side=light_side
+        
+    
