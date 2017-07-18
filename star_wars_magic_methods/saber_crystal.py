d4ef8ab19a58b5e8d35c84d15622135e93e386b4
Jon Handy <jonhandy@gmail.com>
2017-07-18 05:28:15 +0000

    first commit

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,18 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        
+        self.color = color
+        
+        self.red = color[0]
+        self.green = color[1]
+        self.blue = color[2]
+        
+    def __add__(self, other):
+        col = []
+        for i in range(len(self.color)):
+            col.append(self.color[i] + other.color[i])
+        return tuple(col)
+    
+        
+        
+    
