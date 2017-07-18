a39570af9e2df24eb12b762f0a3924e1382d692f
Blithe Brandon <blithe.brandon@gmail.com>
2017-07-18 02:56:04 +0000

    Add readme comment to SaberCrystal class

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,14 @@
 class SaberCrystal(object):
+    """
+    SaberCrystal
+    Init should take one optional parameter, color, which
+    defaults to (255,0,0), which is a tuple representing red,
+    green, blue. There should be 3 instance attributes, red,
+    green, and blue. There needs to also be a *property* 'color'
+    that returns a tuple with the values of red, green, and blue
+    Finally you need to implement methods for the following
+    operators: addition '+', in place addition '+=', subtraction
+    '-', in place subtraction '-=', and contains 'x in y'
+    """
     def __init__(self, color=(255,0,0)):
         pass
