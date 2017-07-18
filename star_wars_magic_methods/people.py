bee6f6c113c3863422e4b915f6c6a849168e1d08
Blithe Brandon <blithe.brandon@gmail.com>
2017-07-18 02:51:19 +0000

    Add readme comment to People class

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,12 @@
 class People(object):
+    """
+    People
+    The init for the people class should take two parameters:
+    name and dark_side (this one is an optional parameter that
+    should have a default value of False) You need to implement
+    methods for the following operators: callable(x()), /, *, <<,
+    >>, negative(-x), positive(+x), invert(~x), ^, == You should
+    also have two properties light_side and dark_side
+    """
     def __init__(self, name, dark_side=False):
         pass
