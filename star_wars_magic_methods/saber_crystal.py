3e16e7e2338b349c78ac3ec39f3d373a229f1772
Marouane Ab <marouane.abra@gmail.com>
2017-07-19 20:38:30 +0000

    added working contains func

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -22,3 +22,9 @@ class SaberCrystal(object):
             return SaberCrystal(color = (self.red-other.red, self.green-other.green, self.blue-other.blue))
         else:
             return SaberCrystal(color = (self.red-other[0], self.green-other[1],self.blue-other[2]))
+            
+    def __contains__(self, other):
+        if (isinstance(other, SaberCrystal)):
+            return(self.red >= other.red and self.green >= other.green and self.blue >= other.blue)
+        else:
+            return(self.red >= other[0] and self.green >= other[1] and self.blue >= other[2])
