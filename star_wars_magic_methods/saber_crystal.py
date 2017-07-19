cd228a5ef369d9260b5b88c25366b2f666a0c263
Marouane Ab <marouane.abra@gmail.com>
2017-07-19 20:27:22 +0000

    added working add magic mthd

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,6 +1,20 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        self.red=255
-        self.green=0
-        self.blue=0
-        self.color=color
+        self.red=color[0]
+        self.green=color[1]
+        self.blue=color[2]
+    
+    @property
+    def color(self):
+        return (self.red, self.green, self.blue)
+        
+    def __eq__(self,other):
+        return self.color == other.color
+    
+    def __add__(self,other):
+        if isinstance(other,SaberCrystal):
+            return SaberCrystal(color = (self.red+other.red, self.green + other.green, self.blue + other.blue))
+        else:
+            return SaberCrystal(color = (self.red+other[0], self.green+other[1],self.blue+other[2]))
+            
+
