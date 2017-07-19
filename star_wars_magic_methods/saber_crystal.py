462c61207a31016e4d3b543685eb2b1bf945dfaf
Marouane Ab <marouane.abra@gmail.com>
2017-07-19 20:47:52 +0000

    added both the in place functions

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -28,3 +28,15 @@ class SaberCrystal(object):
             return(self.red >= other.red and self.green >= other.green and self.blue >= other.blue)
         else:
             return(self.red >= other[0] and self.green >= other[1] and self.blue >= other[2])
+            
+    def __iadd__(self,other):
+        if isinstance(other,SaberCrystal):
+            return SaberCrystal(color = (self.red+other.red, self.green + other.green, self.blue + other.blue))
+        else:
+            return SaberCrystal(color = (self.red+other[0], self.green+other[1],self.blue+other[2]))            
+
+    def __isub__(self,other):
+        if isinstance(other,SaberCrystal):
+            return SaberCrystal(color = (self.red-other.red, self.green-other.green, self.blue-other.blue))
+        else:
+            return SaberCrystal(color = (self.red-other[0], self.green-other[1],self.blue-other[2]))
