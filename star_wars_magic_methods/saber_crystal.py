c8bfcf17efc9a9993a1f2df1475703dbbea51249
Conor Kennedy <conork369@gmail.com>
2017-07-20 18:39:09 +0000

    done

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,42 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self.red = color[0]
+        self.green = color[1]
+        self.blue = color[2]
+        
+    @property
+    def color(self):
+        return(self.red, self.green, self.blue)
+    
+    def __add__(self, other):
+        if isinstance(other, SaberCrystal):
+            return SaberCrystal(color = (self.red + other.red, self.green + other.green, self.blue + other.blue))
+        else:
+            return SaberCrystal(color = (self.red+other[0], self.green+other[1],self.blue+other[2]))
+            
+    def __sub__(self, other):
+        if isinstance(other, SaberCrystal):
+            return SaberCrystal(color = (self.red - other.red, self.green - other.green, self.blue - other.blue))
+        else:
+            return SaberCrystal(color = (self.red - other[0], self.green - other[1], self.blue - other[2]))
+    def __iadd__(self, other):
+        if isinstance(other, SaberCrystal):
+            return SaberCrystal(color = (self.red + other.red, self.green + other.green, self.blue + other.blue))
+        else:
+            return SaberCrystal(color = (self.red + other[0], self.green + other[1], self.blue + other[2]))
+    
+    def __isub__(self, other):
+        if isinstance(other, SaberCrystal):
+            return SaberCrystal(color = (self.red - other.red, self.green - other.green, self.blue - other.blue))
+        else:
+            return SaberCrystal(color = (self.red - other[0], self.green - other[1], self.blue - other[2]))
+            
+    def __contains__(self, other):
+        if (isinstance(other, SaberCrystal)):
+            return(self.red >= other.red and self.green >= other.green and self.blue >= other.blue)
+        else:
+            return(self.red >= other[0] and self.green >= other[1] and self.blue >= other[2])
+
+    
+    def __eq__(self, other):
+        return self.color == other.color        
