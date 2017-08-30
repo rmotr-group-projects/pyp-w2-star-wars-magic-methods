bfea59f97e4ca6d74d6aa9a8e5c6b9d461d8ac72
Reina Chano Murray <r.chano@gmail.com>
2017-08-30 21:59:59 +0000

    more changes to saber_crystal.py

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -14,17 +14,37 @@ class SaberCrystal(object):
         else:
             raise TypeError()
         
-    def __add__(self, other):
+    def __contains__(self, other):
         if isinstance(other, SaberCrystal):
-            red = self.red + other.red
-            green = self.green + other.green
-            blue = self.blue + other.blue
-            return (red, green, blue)
-        
-        elif (isinstance(other, tuple) and len(other) == 3):
-            red = self.red + other[0]
-            green = self.green + other[1]
-            blue = self.blue + other[2]
+            red_statement = (self.red == other.red if self.red > 0 else False)
+            green_statement = (self.green == other.green if self.green > 0 else False)
+            blue_statement = (self.blue == other.blue if self.blue > 0 else False)
+            
+            return red_statement or blue_statement or green_statement
+
+        elif isinstance(other, tuple):
+            red_statement = (self.red == other[0] if self.red > 0 else False)
+            green_statement = (self.green == other[1] if self.green > 0 else False)
+            blue_statement = (self.blue == other[2] if self.blue > 0 else False)
+
+            return red_statement or blue_statement or green_statement
         
         else:
-            raise TypeError
+            raise TypeError()
+        
+        
+'''    def __add__(self, *args):
+        if isinstance(args, SaberCrystal):
+            red = self.red + args.red
+            green = self.green + args.green
+            blue = self.blue + args.blue
+            return (red, green, blue)
+        
+        elif (isinstance(args, tuple) and len(args) == 3):
+            red = self.red + args[0]
+            green = self.green + args[1]
+            blue = self.blue + args[2]
+            return (red, green, blue)
+        
+        else:
+            raise TypeError'''
