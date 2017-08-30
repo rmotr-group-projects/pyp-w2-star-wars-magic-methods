77b467550b42d367c3abc9093b97e499b1f4ab91
Reina Chano Murray <r.chano@gmail.com>
2017-08-30 21:14:14 +0000

    solved people.py, working on saber_crystal.py

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,59 @@
 class People(object):
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self.dark_side = dark_side
+        self.light_side = not dark_side
+        
+    def __str__(self):
+        return self.name
+    
+    def __call__(self):
+        return "Help me {}, you're my only hope.".format(self)
+    
+    def __div__(self, other):
+        if isinstance(other, People):
+            return "{} swings a lightsaber at {}.".format(self, other)
+        raise TypeError()
+
+
+    def __mul__(self, other):
+        if isinstance(other, People):
+            return "{} throws a thermal detonator at {}!".format(self, other)
+        raise TypeError()
+    
+    def __rshift__(self, other):
+        if isinstance(other, People):
+            return "{} uses the force to push {} away from them.".format(self, other)
+        raise TypeError()
+
+    def __lshift__(self, other):
+        if isinstance(other, People):
+            return "{} uses the force to pull {} towards them.".format(self, other)
+        raise TypeError()
+
+    
+    def __neg__(self):
+        self.dark_side = True
+        self.light_side = False
+
+    def __pos__(self):
+        self.light_side = True
+        self.dark_side = False
+    
+    def __invert__(self):
+        self.dark_side = not self.dark_side
+        self.light_side = not self.light_side
+
+    def __xor__(self, other):
+        if isinstance(other, People):
+            return "{} force chokes {}.".format(self, other)
+        raise TypeError()
+
+    
+    def __eq__(self, other):
+        if isinstance(other, People):
+            if other.name == 'Han Solo':
+                return "{} shoots {}. BECAUSE HAN SHOOTS FIRST.".format(other, self)
+            else:
+                return "{} shoots {}.".format(self, other)
+        raise TypeError()
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,30 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self.color = color
+        self.red = color[0]
+        self.green = color[1]
+        self.blue = color[2]
+    
+    def __str__(self):
+        return self.color
+    
+    def __eq__(self, other):
+        if isinstance(other, SaberCrystal):
+            return self.color == other.color
+        else:
+            raise TypeError()
+        
+    def __add__(self, other):
+        if isinstance(other, SaberCrystal):
+            red = self.red + other.red
+            green = self.green + other.green
+            blue = self.blue + other.blue
+            return (red, green, blue)
+        
+        elif (isinstance(other, tuple) and len(other) == 3):
+            red = self.red + other[0]
+            green = self.green + other[1]
+            blue = self.blue + other[2]
+        
+        else:
+            raise TypeError
