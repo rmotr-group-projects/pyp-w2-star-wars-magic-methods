5ed616f54ba65ebe926cb8431d4dd05c7ace7596
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-10-13 21:07:36 +0000

    people.py 4 out of 12 tests completed

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,24 @@
 class People(object):
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self._dark_side = dark_side
+        
+    @property
+    def light_side(self):
+        return not self._dark_side
+    
+    @property
+    def dark_side(self):
+        return self._dark_side
+    
+    def __str__(self):
+        return self.name
+        
+    def __call__(self):
+        return "Help me {}, you're my only hope.".format(self.name)
+        
+    def __div__(self, other):
+        if type(self) is not People or type(other) is not People:
+            raise TypeError()
+        return "{} swings a lightsaber at {}.".format(self.name, other.name)
+        
