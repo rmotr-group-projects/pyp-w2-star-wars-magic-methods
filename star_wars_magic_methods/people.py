787f662cb960fa86393664753fd7c0c71f807a1d
benny1979 <aldridge.b@googlemail.com>
2017-10-16 07:30:18 +0000

    All tests passing

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,78 @@
 class People(object):
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self._ds = dark_side
+        
+    @property
+    def dark_side(self):
+            return self._ds
+            
+    @dark_side.setter
+    def dark_side(self, state):
+        self._ds = state
+        
+    @property
+    def light_side(self):
+        return not self.dark_side
+        
+    def __str__(self):
+        return self.name
+        
+    def __call__(self):
+        return 'Help me {}, you\'re my only hope.'.format(self.name)
+    
+    def __truediv__(self, other):
+        try:
+            isinstance(other, People)
+            return '{} swings a lightsaber at {}.'.format(self.name, other.name) #
+        except:
+            raise TypeError()
+            #is it good enough to stop here?
+        
+    def __mul__(self, other):
+        try:
+            isinstance(other, People)
+            return '{} throws a thermal detonator at {}!'.format(self.name, other.name) 
+        except:
+            raise TypeError()
+
+    def __lshift__(self, other):
+        try: #type checking not required here to pass tests but keep for consistency
+            isinstance(other, People)
+            return '{} uses the force to pull {} towards them.'.format(self.name, other.name) 
+        except:
+            raise TypeError() 
+
+    def __rshift__(self, other):
+        try:
+            isinstance(other, People)
+            return '{} uses the force to push {} away from them.'.format(self.name, other.name) 
+        except:
+            raise TypeError()
+    
+    def __neg__(self):
+        if self.dark_side == False:
+            ~self
+        
+    def __pos__(self): 
+        if self.dark_side == True:
+            ~self
+    
+    def __invert__(self):
+        self.dark_side = not self.dark_side
+        
+    def __xor__(self, other):
+        try: #type checking not required here to pass tests but keep for consistency
+            isinstance(other, People)
+            return '{} force chokes {}.'.format(self.name, other.name) 
+        except:
+            raise TypeError() 
+    
+    def __eq__(self, other): 
+        try: #type checking not required here to pass tests but keep for consistency
+            isinstance(other, People)
+            if other.name == 'Han Solo':
+                return 'Han Solo shoots {}. BECAUSE HAN SHOOTS FIRST.'.format(self.name)
+            return '{} shoots {}.'.format(self.name, other.name) 
+        except:
+            raise TypeError() 
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,65 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        checked_color = self.color_check(color) # not required for tests but may as well pop in
+        self.red, self.green, self.blue = checked_color
+    
+    @staticmethod
+    def color_check(tuple_in):
+        # this seems a tad opaque though it does the trick
+        return tuple(map(lambda x: max(min(255, x), 0), tuple_in))
+    
+    @staticmethod
+    def extract_color(input): # either put the check up here
+        if isinstance(input, SaberCrystal):
+            return input.color
+        else: 
+            return input
+
+    @property
+    def color(self):
+        return (self.red, self.green, self.blue)
+        
+    @color.setter
+    def color(self, color_in):
+        checked_color = self.color_check(color_in)
+        self.red, self.green, self.blue = checked_color
+    
+    def __eq__(self, other):
+        for i in range(3):
+            if self.color[i] != other.color[i]:
+                return False
+        return True
+
+    def add_color(self, other, adj = 1):
+        # QUESTION: eventually i've opted for putting the logic up here because it lets me not have to repeat in __add__ and __iadd__
+        # does that sound like a good idea or no?
+        other_color = self.extract_color(other)
+        if adj == -1:
+            other_color = tuple([-item for item in other_color])
+        added_color = tuple([sum(col) for col in list(zip(self.color, other_color))])
+        checked_color = self.color_check(added_color)
+        return checked_color
+
+    def __add__(self, other, adj = 1):
+        checked_color = self.add_color(other, adj)
+        return SaberCrystal(checked_color)
+        
+    def __iadd__(self, other, adj = 1):
+        checked_color = self.add_color(other, adj)
+        self.color = checked_color
+        return self
+        #QUESTION: I don't quite understand why we need to return self - though i appreciate it doesn't work without it
+        
+    def __sub__(self, other):
+        return self.__add__(other, -1)
+        
+    def __isub__(self, other):
+        return self.__iadd__(other, -1)
+        
+    def __contains__(self, item):
+        item_color = self.extract_color(item)
+        for i in range(3):
+            if self.color[i] > 0 and self.color[i] == item_color[i]:
+                return True
+        return False
+    
