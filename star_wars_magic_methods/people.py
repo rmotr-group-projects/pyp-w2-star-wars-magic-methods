66342ddcdfeb2902fc31b6f545a87dfaaeb976c8
Karis Shang <kyshang@gmail.com>
2017-10-17 02:43:27 +0000

    completed project

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,84 @@
 class People(object):
+
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self.dark = dark_side
+       
+    def __call__(self):
+        return 'Help me {sName}, you\'re my only hope.'.format(
+                sName = self.name)
+    
+    def __neg__(self):
+        self.dark = True
+    
+    def __pos__(self):
+        self.dark = False
+    
+    def __invert__(self):
+        if self.dark == False:
+            self.dark = True
+        else:
+            self.dark = False
+    
+    def __str__(self):
+        return str(self.name)
+    
+    def __truediv__(self, other):
+        if isinstance(other, People):
+            return '{sName} swings a lightsaber at {oName}.'.format(
+                sName = self.name,
+                oName = other.name)
+        else:
+            raise TypeError
+            
+    __div__ = __truediv__
+        
+    def __mul__(self, other):
+        if isinstance(other, People):
+            return '{sName} throws a thermal detonator at {oName}!'.format(
+                sName = self.name,
+                oName = other.name)
+        else:
+            raise TypeError
+            
+    def __rshift__(self, other):
+        if isinstance(other, People):
+            return '{sName} uses the force to push {oName} away from them.'.format(
+                sName = self.name,
+                oName = other.name)
+        else:
+            raise TypeError
+
+    def __lshift__(self, other):
+        if isinstance(other, People):
+            return '{sName} uses the force to pull {oName} towards them.'.format(
+                sName = self.name,
+                oName = other.name)
+        else:
+            raise TypeError
+    
+    def __eq__(self, other):
+        if other.name == 'Han Solo':
+            return '{oName} shoots {sName}. BECAUSE HAN SHOOTS FIRST.'.format(
+                sName = self.name,
+                oName = other.name)
+        else:
+            return '{sName} shoots {oName}.'.format(
+                sName = self.name,
+                oName = other.name)
+    
+    def __xor__(self, other):
+        return '{sName} force chokes {oName}.'.format(
+            sName = self.name,
+            oName = other.name)
+    
+    @property
+    def light_side(self):
+        if self.dark == False:
+            return True
+        else:
+            return False
+    
+    @property
+    def dark_side(self):
+        return self.dark
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,45 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self.color = color
+
+    def __add__(self,other):
+        if isinstance(other, SaberCrystal):
+            add_crystal = tuple(map(lambda x,y: min(x + y, 255), self.color, other.color))
+        else:
+            add_crystal = tuple(map(lambda x,y: min(x + y, 255), self.color, other))
+        return SaberCrystal(color=add_crystal)
+    
+    def __sub__(self,other):
+        if isinstance(other, SaberCrystal):
+            sub_crystal = tuple(map(lambda x,y: x - y, self.color, other.color))
+        else:
+            sub_crystal = tuple(map(lambda x,y: x - y, self.color, other))
+        return SaberCrystal(color=sub_crystal)
+    
+    def __eq__(self,other):
+        if self.color == other.color:
+            return True
+        else:
+            return False
+    
+    def __contains__(self,other):
+        sub_crystal = self - other
+        if any(x < 0 for x in sub_crystal.color):
+            return False
+        else:
+            return True
+    
+    @property
+    def red(self):
+        return self.color[0]
+    
+    @property
+    def green(self):
+        return self.color[1]
+        
+    @property
+    def blue(self):
+        return self.color[2]
+    
+    
+    
