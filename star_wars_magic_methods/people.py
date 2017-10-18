4467b9da92299dbb3d1cd371c9af55b247a8a463
laurauzcategui <laura.uzcateguij@gmail.com>
2017-10-18 23:41:59 +0100

    Completing the 2nd project

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,74 @@
 class People(object):
+    shoots = False
+
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self._dark_side = dark_side
+        self._light_side = not dark_side
+
+    # def property light side
+    @property
+    def light_side(self):
+        return self._light_side
+
+    @light_side.setter
+    def light_side(self,value):
+        self._light_side = value
+        self._dark_side = not value
+
+    # def
+    @property
+    def dark_side(self):
+        return self._dark_side
+
+    @dark_side.setter
+    def dark_side(self,value):
+        self._dark_side = value
+        self._light_side = not value
+
+    # multiplication
+    def __mul__(self,other):
+        if isinstance(other,People):
+            return '{} {} {}!'.format(self.name, "throws a thermal detonator at", other.name)
+        raise TypeError
+
+    # division
+    def __truediv__(self,other):
+        if isinstance(other,People):
+            return '{} {} {}.'.format(self.name,'swings a lightsaber at',other.name)
+        raise TypeError
+    # negative number
+    def __neg__(self):
+        self.dark_side = True
+
+    # positive number
+    def __pos__(self):
+        self.dark_side = False
+
+    # invert
+    def __invert__(self):
+        previous = self.dark_side
+        self.dark_side = not previous
+
+    def __eq__(self,other):
+        if not People.shoots:
+            People.shoots = True
+            return '{} {} {}.'.format(self.name,'shoots', other.name)
+        return '{} {} {}. {}.'.format(other.name,'shoots', self.name,'BECAUSE HAN SHOOTS FIRST')
+
+    def __call__(self):
+        return '{} {}, {}.'.format('Help me',self.name,"you're my only hope")
+
+    def __xor__(self,other):
+        return '{} {} {}.'.format(self.name, 'force chokes', other.name)
+
+    def __lshift__(self,other):
+        return '{} {} {} {}.'.format(self.name, 'uses the force to pull', other.name, 'towards them')
+
+    def __rshift__(self,other):
+        if isinstance(other,People):
+            return '{} {} {} {}.'.format(self.name, 'uses the force to push', other.name,'away from them')
+        raise TypeError
+
+    def __str__(self):
+        return self.name
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,45 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self._red = color[0]
+        self._green = color[1]
+        self._blue = color[2]
+
+    @property
+    def color(self):
+        return (self._red,self._green,self._blue)
+
+    @color.setter
+    def color(self,value):
+        self._red = value[0]
+        self._green = value[1]
+        self._blue = value[2]
+
+    def __eq__(self,other):
+        return self.color == other.color
+
+    def __add__(self,other):
+        r1,g1,b1 = self.color if isinstance(self,SaberCrystal) else self
+        r2,g2,b2 = other.color if isinstance(other,SaberCrystal) else other
+        return SaberCrystal(color=(r1+r2,g1+g2,b1+b2))
+
+    def __iadd__(self,other):
+        r1,g1,b1 = self.color if isinstance(self,SaberCrystal) else self
+        r2,g2,b2 = other.color if isinstance(other,SaberCrystal) else other
+        self.color = (r1+r2,g1+g2,b1+b2)
+
+    def __sub__(self,other):
+        r1,g1,b1 = self.color if isinstance(self,SaberCrystal) else self
+        r2,g2,b2 = other.color if isinstance(other,SaberCrystal) else other
+        return SaberCrystal(color=(r1-r2,g1-g2,b1-b2))
+
+    def __isub__(self,other):
+        r1,g1,b1 = self.color if isinstance(self,SaberCrystal) else self
+        r2,g2,b2 = other.color if isinstance(other,SaberCrystal) else other
+        self.color = (r1-r2,g1-g2,b1-b2)
+
+white_crystal = SaberCrystal(color=(255,255,255))
+red_crystal = SaberCrystal()
+print(white_crystal.color)
+print(red_crystal.color)
+red_crystal += white_crystal
+#assert red_crystal == white_crystal
