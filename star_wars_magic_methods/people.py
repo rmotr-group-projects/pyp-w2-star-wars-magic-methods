30156b0768b0d114b38268488eb711ee8ca4f106
Brian <brachielles@gmail.com>
2017-10-19 23:33:50 +0000

    fixed failing travis

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -19,7 +19,7 @@ class People(object):
     def __call__(self):
         return "Help me {}, you're my only hope.".format(self)
     
-    def __div__(self, other):
+    def __truediv__(self, other):
         if isinstance(other, People):
             return '{} swings a lightsaber at {}.'.format(self, other)
         else:
@@ -64,3 +64,4 @@ class People(object):
         else:
             return '{} shoots {}.'.format(self, other)
     
+    __div__ = __truediv__
