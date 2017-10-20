1a85a77d59ebfa90d5bc9f3294889ecb631de1a5
Kelli Blalock <kelli.blalock2@gmail.com>
2017-10-20 10:16:14 +0000

    Pass first 3 SaberCrystal tests

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,39 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self.color = color
+
+    def __eq__(self, other):
+        if not isinstance(other, SaberCrystal):
+            return False
+        return self.color == other.color
+
+    def __add__(self, other):
+        other_color = other
+        if isinstance(other, tuple):
+            other_crystal = SaberCrystal(color=other)
+        else:
+            if not isinstance(other, SaberCrystal):
+                raise TypeError("Can't perform operation with SaberCrystal "
+                                "object and {}".format(type(other).__name__))
+            other_crystal = other
+        result_crystal = SaberCrystal(color = (self.red + other_crystal.red,
+                                      self.green + other_crystal.green,
+                                      self.blue + other_crystal.blue))
+        return result_crystal
+
+    def __iadd__(self, other):
+        print self, other
+        return self + (self + other)
+
+    @property
+    def red(self):
+        return self.color[0]
+
+    @property
+    def green(self):
+        return self.color[1]
+
+    @property
+    def blue(self):
+        return self.color[2]
+
