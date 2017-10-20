e54e518774c7630092ec81debb5f904c14ae30d1
Kelli Blalock <kelli.blalock2@gmail.com>
2017-10-20 10:13:41 +0000

    Make light_side a property

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -2,7 +2,10 @@ class People(object):
     def __init__(self, name, dark_side=False):
         self.name = name
         self.dark_side = dark_side
-        self.light_side = not dark_side
+
+    @property
+    def light_side(self):
+        return not self.dark_side
 
     def __str__(self):
         return self.name
@@ -38,15 +41,12 @@ class People(object):
 
     def __neg__(self):
         self.dark_side = True
-        self.light_side = False
 
     def __pos__(self):
         self.dark_side = False
-        self.light_side = True
 
     def __invert__(self):
         self.dark_side = not self.dark_side
-        self.light_side = not self.light_side
 
     def __xor__(self, other):
         if not isinstance(other, People):
@@ -61,6 +61,6 @@ class People(object):
         if other.name == 'Han Solo':
             return "Han Solo shoots {}. BECAUSE HAN SHOOTS FIRST.".format(self)
         return "{} shoots {}.".format(self, other)
-
+        
     # Alias for Python 2 Compatibility
     __div__ = __truediv__
