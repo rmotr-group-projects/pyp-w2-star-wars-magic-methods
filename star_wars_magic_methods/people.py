f409ba08196a5a1e5e8c79466052a00e1bfa70e1
AllenAnthes <abanthes@gmail.com>
2017-11-22 21:10:04 -0600

    All the dunders

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,65 @@
 class People(object):
     def __init__(self, name, dark_side=False):
-        pass
+        self.__name = name
+        self.__dark_side = dark_side
+
+    @property
+    def light_side(self):
+        return self.__dark_side is False
+
+    @property
+    def dark_side(self):
+        return self.__dark_side is True
+
+    @property
+    def name(self):
+        return self.__name
+
+    def __str__(self):
+        return self.name
+
+    def __call__(self, *args, **kwargs):
+        return "Help me {}, you're my only hope.".format(self.name)
+
+    def __div__(self, other):
+        if type(other) is People:
+            return "{} swings a lightsaber at {}.".format(self.name, other.name)
+        else:
+            raise TypeError
+
+    __truediv__ = __div__
+
+    def __mul__(self, other):
+        if type(other) is People:
+            return "{} throws a thermal detonator at {}!".format(self.name, other.name)
+        else:
+            raise TypeError
+
+    def __rshift__(self, other):
+        if type(other) is People:
+            return "{} uses the force to push {} away from them.".format(self.name, other.name)
+        else:
+            raise TypeError
+
+    def __lshift__(self, other):
+        if type(other) is People:
+            return "{} uses the force to pull {} towards them.".format(self.name, other.name)
+        else:
+            raise TypeError
+
+    def __neg__(self):
+        self.__dark_side = True
+
+    def __pos__(self):
+        self.__dark_side = False
+
+    def __invert__(self):
+        self.__dark_side = not self.__dark_side
+
+    def __xor__(self, other):
+        return "{} force chokes {}.".format(self.name, other.name)
+
+    def __eq__(self, other):
+        if self.name == "Greedo" and other.name == "Han Solo":
+            return 'Han Solo shoots Greedo. BECAUSE HAN SHOOTS FIRST.'
+        return "{} shoots {}.".format(self.name, other.name)
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,28 @@
 class SaberCrystal(object):
-    def __init__(self, color=(255,0,0)):
-        pass
+    def __init__(self, color=(255, 0, 0)):
+        self.red = color[0]
+        self.green = color[1]
+        self.blue = color[2]
+
+    @property
+    def color(self):
+        return self.red, self.green, self.blue
+
+    def __eq__(self, other):
+        return self.red == other.red and self.green == other.green and self.blue == other.blue
+
+    def __add__(self, other):
+        return SaberCrystal(
+            color=[min(255, x + y) for x, y in zip(self.color, other.color if type(other) is SaberCrystal else other)])
+
+    def __sub__(self, other):
+        return SaberCrystal(
+            color=[max(0, x - y) for x, y in zip(self.color, other.color if type(other) is SaberCrystal else other)])
+
+    def __contains__(self, item):
+        if type(item) is SaberCrystal:
+            item = item.color
+        for index, val in enumerate(item):
+            if val != 0 and self.color[index] != val:
+                return False
+        return True
