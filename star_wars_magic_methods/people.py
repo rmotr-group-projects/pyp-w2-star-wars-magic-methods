793a51f8757c923604722ce0c3a0a3313e879c6d
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-11-26 15:24:34 -0500

    implement light_side property, str(), and /

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,19 @@
 class People(object):
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self.dark_side = dark_side
+
+    @property
+    def light_side(self):
+        return True if not self.dark_side else self.dark_side
+
+    def __str__(self):
+        return self.name
+
+    def __truediv__(self, other):
+
+        if type(other) is float:
+            raise TypeError
+
+        # ???: How does method know that other is using the name attribute from the class?
+        return "%s swings a lightsaber at %s." % (self.name, other)
