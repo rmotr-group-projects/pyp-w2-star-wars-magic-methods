5f00751675108e7024502e55063f3439fae624df
Alara Dirik <alaradirik@gmail.com>
2017-11-27 19:02:01 +0000

    First commit

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,61 @@
 class People(object):
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self.dark = dark_side
+        
+    def __str__(self):
+        return self.name
+        
+    @property
+    def dark_side(self):
+        return self.dark
+    
+    @property
+    def light_side(self):
+        return not self.dark
+
+    def __call__(self):
+        return "Help me {self}, you're my only hope.".format(self = self)
+    
+    def __div__(self, other):
+        if not isinstance(other, People):
+            raise TypeError
+        return "{self} swings a lightsaber at {other}.".format(self = self, other = other)
+        
+    __truediv__ = __div__
+    
+    def __mul__(self, other):
+        if isinstance(other, People):
+            return "{self} throws a thermal detonator at {other}!".format(self = self, other = other)
+        else:
+            raise TypeError
+        
+    def __rshift__(self, other):    
+        if not isinstance(other, People):
+            raise TypeError
+        return '{self} uses the force to push {other} away from them.'.format(self = self, other = other)
+        
+    def __lshift__(self, other):
+        if not isinstance(other, People):
+            raise TypeError() 
+        return '{self} uses the force to pull {other} towards them.'.format(self = self, other = other)
+
+    def __neg__(self):
+       self.dark = True  
+       
+    def __pos__(self):
+        self.dark = False
+    
+    def __invert__(self):
+        self.dark = not self.dark
+            
+    def __eq__(self, other): 
+        if not isinstance(other, People):
+            raise TypeError()
+        if other.name == 'Han Solo':
+            return 'Han Solo shoots {self}. BECAUSE HAN SHOOTS FIRST.'.format(self = self)
+        else:
+            return '{self} shoots {other}.'.format(self = self, other = other) 
+
+    def __xor__(self, other):
+        return '{self} force chokes {other}.'.format(self = self, other = other)
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,44 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self.red = color[0]
+        self.green = color[1]
+        self.blue = color[2]
+    
+    @property
+    def color(self):
+        return (self.red, self.green, self.blue)
+    
+    def __eq__(self, other):
+        return self.color == other.color
+    
+    def __add__(self, other):
+        if not isinstance(other, SaberCrystal):
+            other = SaberCrystal(other)
+            
+        new_color = [0,0,0]
+        for i in range(0, 3):
+            new_color[i] = min(self.color[i] + other.color[i], 255)
+  
+        return SaberCrystal(tuple(new_color))
+    
+    def __iadd(self, other):
+        return self.__add__(other)
+        
+    def __sub__(self, other):
+        if not isinstance(other, SaberCrystal):
+            other = SaberCrystal(other)
+            
+        new_color = [0,0,0]
+        for i in range(0, 3):
+            new_color[i] = max(self.color[i] - other.color[i], 0)
+  
+        return SaberCrystal(tuple(new_color))
+    
+    def __isub(self, other):
+        return self.__sub__(other)
+        
+    def __contains__(self, other):
+        if not isinstance(other, SaberCrystal):
+            other = SaberCrystal(color=other)
+
+        return self.red >= other.red and self.green >= other.green and self.blue >= other.blue
