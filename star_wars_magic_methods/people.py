bd7cc7ec19dc0a735fe042176d08b251bcd6fa4a
J F <jordi.frank1994@gmail.com>
2017-11-29 03:44:39 +0000

    Final changes to star wars project.

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,71 @@
 class People(object):
-    def __init__(self, name, dark_side=False):
-        pass
+    def __init__(self, name, dark_side=False, light_side=True):
+        self.name = name 
+        self.dark_side = dark_side 
+        self.light_side = light_side
+        if dark_side == True:
+            self.light_side = False
+    
+    def __str__(self):
+        return '%s' % self.name 
+    
+    def __call__(self):
+        if self.name == 'Obi-Wan Kenobi':
+            return "Help me Obi-Wan Kenobi, you're my only hope."
+    
+    def __div__(self, other): 
+        try:
+            return "%s swings a lightsaber at %s." % (self.name, other.name)
+        except: 
+            raise TypeError()
+            
+    def __mul__(self, other):
+        try:
+            return "%s throws a thermal detonator at %s!" % (self.name, other.name)
+        except: 
+            raise TypeError()
+    
+    def __rshift__(self, other):
+        try: 
+            return "%s uses the force to push %s away from them." % (self.name, other.name)
+        except:
+            raise TypeError()
+        
+    def __lshift__(self, other):
+        try:
+            return "%s uses the force to pull %s towards them." % (self.name, other.name)
+        except:
+            raise TypeError()
+    
+    def __pos__(self):
+        if self.dark_side == True:
+            self.dark_side = False
+            self.light_side = True
+            
+    def __neg__(self):
+        if self.light_side == True and self.dark_side == False:
+            self.dark_side = True
+            self.light_side = False
+    
+    def __invert__(self):
+        if self.dark_side == True and self.light_side == False: 
+            self.dark_side = False 
+            self.light_side = True
+        else: 
+            self.dark_side = True
+            self.light_side = False
+    
+    def __xor__(self, other): 
+        try:
+            return "%s force chokes %s." % (self.name, other.name) 
+        except:
+            raise TypeError() 
+    
+    def __eq__(self, other):
+        try: 
+            if other.name == "Han Solo": 
+                return "%s shoots %s. BECAUSE HAN SHOOTS FIRST." % (other.name, self.name)
+            else:
+                return "%s shoots %s." % (self.name, other.name)
+        except:
+            raise TypeError()
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,81 @@
 class SaberCrystal(object):
-    def __init__(self, color=(255,0,0)):
-        pass
+    def __init__(self, color=(255,0,0), red=255, green=0, blue=0):
+        self.color = color 
+        self.red = 255
+        self.green = 0 
+        self.blue = 0 
+    
+    def __eq__(self, other):
+        return self.color == other.color
+    
+    def __add__(self, other):
+        lst = []
+        for el in range(0,3):
+            if isinstance(other, tuple): 
+                lst.append(self.color[el] + other[el])
+            else: 
+                lst.append(self.color[el] + other.color[el])
+        lst = [255 if el > 255 else el for el in lst]
+        return SaberCrystal(tuple(lst))
+    
+    def __iadd__(self, other):
+        lst = []
+        for el in range(0,3):
+            if isinstance(other, tuple):
+                value = self.color[el] + other[el]
+                lst.append(value)
+            else:
+                value = self.color[el] + other.color[el]
+                lst.append(value)
+        lst = [255 if el > 255 else el for el in lst]
+        return SaberCrystal(tuple(lst))
+    
+    def __sub__(self, other):
+        lst = []
+        for el in range(0,3):
+            if isinstance(other, tuple):
+                lst.append(self.color[el] - other[el])
+            else:
+                lst.append(self.color[el] - other.color[el])
+        return SaberCrystal(tuple(lst))
+
+    def __isub__(self, other):
+        lst = []
+        for el in range(0,3):
+            if isinstance(other, tuple):
+                value = self.color[el] - other[el]
+                lst.append(value)
+            else:
+                value = self.color[el] - other.color[el]
+                lst.append(value)
+        return SaberCrystal(tuple(lst))
+    
+    def __contains__(self, item):
+        if isinstance(item, tuple) and isinstance(self, tuple):
+            for i in range(0, 3):
+                item_idx = [idx for idx, v in enumerate(item) if v == item[i]]
+                self_idx = [idx for idx, v in enumerate(self) if v == self[i]]
+                if self[i] == item[i] and set(item_idx).issubset(self_idx):
+                    return True
+            return False
+        elif isinstance(item, tuple) and isinstance(self, SaberCrystal):
+            for i in range(0, 3):
+                item_idx = [idx for idx, v in enumerate(item) if v == item[i]]
+                self_idx = [idx for idx, v in enumerate(self.color) if v == self.color[i]]
+                if self.color[i] == item[i] and set(item_idx).issubset(self_idx):
+                    return True
+            return False
+        elif isinstance(item, SaberCrystal) and isinstance(self, SaberCrystal):
+            for i in range(0, 3):
+                item_idx = [idx for idx, v in enumerate(item.color) if v == item.color[i]]
+                self_idx = [idx for idx, v in enumerate(self.color) if v == self.color[i]]
+                if self.color[i] == item.color[i] and set(item_idx).issubset(self_idx):
+                    return True
+            return False
+        else:
+            for i in range(0, 3):
+                item_idx = [idx for idx, v in enumerate(item.color) if v == item.color[i]]
+                self_idx = [idx for idx, v in enumerate(self) if v == self.color[i]]
+                if self[i] == item.color[i] and set(item_idx).issubset(self_idx):
+                    return True
+            return False
