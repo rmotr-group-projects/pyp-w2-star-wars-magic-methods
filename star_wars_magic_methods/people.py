1c9866d0e9eaebc0f48c4c28f9eac193920190ee
unoor <usman.noor127@hotmail.com>
2017-11-29 22:17:44 +0000

    first working copy

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,86 @@
+from __future__ import division
+
+
 class People(object):
+
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name 
+        self._dark_side = dark_side
+        if not dark_side:
+            self._light_side = True
+        else:
+            self._light_side = False
+        
+    @property
+    def light_side(self):
+         return self._light_side
+    
+    @property
+    def dark_side(self):
+         return self._dark_side
+    
+    @light_side.setter
+    def light_side(self,value):
+        self._light_side = value    
+    
+    @dark_side.setter
+    def dark_side(self,value):
+        self._dark_side = value
+
+    def __str__(self):
+        return self.name
+    
+    # def __eq__(self, other_string):
+    #     return self.name == other_string
+    
+    def __call__(self):                  
+        return "Help me Obi-Wan Kenobi, you're my only hope."
+
+    def __div__(self,other):
+        if isinstance(other, People) == False: #and isinstance(other, str) == False:
+            raise TypeError
+        return self.name + " swings a lightsaber at "+ other.name + "."
+
+    def __mul__(self,other):
+        if isinstance(other, People) == False: #and isinstance(other, str) == False:
+            raise TypeError
+        return self.name + " throws a thermal detonator at "+ other.name + "!"
+    
+    def __rshift__(self, other):
+        if isinstance(other, People) == False: #and isinstance(other, str) == False:
+            raise TypeError
+        return self.name + " uses the force to push "+ other.name + " away from them."
+
+    def __lshift__(self, other):
+        if isinstance(other, People) == False: #and isinstance(other, str) == False:
+            raise TypeError
+        return self.name + " uses the force to pull "+ other.name + " towards them."
+
+    def __neg__(self):
+        self.light_side = False
+        self.dark_side = True
+
+    def __pos__(self):
+        self.light_side = True
+        self.dark_side =  False
+
+    def __invert__(self):
+        self.light_side = not self.light_side
+
+    def __xor__(self, other):
+        if isinstance(other, People) == False: #and isinstance(other, str) == False:
+            raise TypeError
+        return self.name + " force chokes "+ other.name + "."
+
+    def __eq__(self, other):
+        if isinstance(other, People) == False: #and isinstance(other, str) == False:
+            raise TypeError
+        
+        if self.name.find('Han') > -1:
+            return self.name + " shoots "+ other.name + "."
+        else:
+            return other.name + " shoots "+ self.name + ". BECAUSE HAN SHOOTS FIRST."
+
+
+
+
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,81 @@
+
+from __future__ import division
+
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self.red = color[0]
+        self.green = color[1]
+        self.blue = color[2]
+ 
+         
+    @property
+    def color(self):
+        return (self.red, self.green, self.blue)
+    
+    def __eq__(self,other):
+        match = [(a==b) for a,b  in zip(self.color, other.color)]
+        return all(match)
+        
+    def check_max(self, color_list):
+        return [255 if color > 255 else color for color in color_list ]
+    
+    def __add__(self, other):
+        if isinstance(other, tuple) ==True:
+            color_list = self.check_max(map( lambda x: x[0] + x[1], zip(self.color, other)))
+            return SaberCrystal((tuple(color_list)))
+        else:
+            color_list = self.check_max(map( lambda x: x[0] + x[1], zip(self.color, other.color)))
+            return SaberCrystal((tuple(color_list)))
+
+    def __iadd__(self, other):
+        if isinstance(other, tuple) ==True:
+            color_list = self.check_max(map( lambda x: x[0] + x[1], zip(self.color, other)))
+            return SaberCrystal((tuple(color_list)))
+        else:
+            color_list = self.check_max(map( lambda x: x[0] + x[1], zip(self.color, other.color)))
+            return SaberCrystal((tuple(color_list)))
+
+    def __sub__(self, other):
+        if isinstance(other, tuple) ==True:
+            color_list = self.check_max(map( lambda x: x[0] - x[1], zip(self.color, other)))
+            return SaberCrystal((tuple(color_list)))
+        else:
+            color_list = self.check_max(map( lambda x: x[0] - x[1], zip(self.color, other.color)))
+            return SaberCrystal((tuple(color_list)))
+
+    def __str__(self):
+        return str(self.color)
+        
+        
+    def __isub__(self, other):
+        if isinstance(other, tuple) ==True:
+            color_list = self.check_max(map( lambda x: x[0] - x[1], zip(self.color, other)))
+            return SaberCrystal((tuple(color_list)))
+        else:
+            color_list = self.check_max(map( lambda x: x[0] - x[1], zip(self.color, other.color)))
+            return SaberCrystal((tuple(color_list)))
+
+    def __contains__(self, other):
+        if isinstance(other, tuple) ==True:
+            color_match = [a==b if a != 0 and b!=0 else False for a,b in zip(self.color, other)]
+            return any(color_match)
+        else:
+            color_match = [a==b if a != 0 and b!=0 else False for a,b in zip(self.color, other.color)]
+            return any(color_match)
+        
+
+# red_crystal = SaberCrystal()
+# green = (0,0,0)
+# green_crystal = SaberCrystal(color=green)
+# print(red_crystal in green_crystal)
+
+# # blue = (0,0,255)
+# # blue_crystal = SaberCrystal(color=blue)
+# # white_crystal = SaberCrystal(color=(255,255,255))
+# # self.assertTrue(red_crystal in white_crystal)
+# # self.assertFalse(red_crystal in green_crystal)
+# # self.assertTrue(green in white_crystal)
+# # self.assertFalse(blue in red_crystal)
+
+
+
