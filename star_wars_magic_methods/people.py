195c968cbeb9f37d9d79b5aff9064e87a03c5338
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-11-29 22:47:27 +0000

    Add __init__ and properties to People class

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,34 @@
 class People(object):
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self._dark_side = dark_side
+    
+    @property
+    def dark_side(self):
+        return self._dark_side
+    
+    @dark_side.setter
+    def dark_side(self, ds_value):
+        self._dark_side = ds_value
+     
+    @property
+    def light_side(self):
+        return not self.dark_side
+    
+    @light_side.setter
+    def light_side(self, value):
+        self._light_side = not value
+
+
+        
+        # def call():
+        #     return 'Help me ' + self.name + ', you\'re my only hope.'
+    
+    
+        def __str__():
+            return self.name
+        
+        
+            
+        
+        
