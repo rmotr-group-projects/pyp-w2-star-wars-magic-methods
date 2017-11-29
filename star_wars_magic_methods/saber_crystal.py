22625764996a23e47df501fa1412c62ea62fa000
AllenAnthes <abanthes@gmail.com>
2017-11-29 16:14:05 -0600

    more code cleaning, swapped type with isinstance

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -11,20 +11,33 @@ class SaberCrystal(object):
     def __eq__(self, other):
         if not isinstance(other, SaberCrystal):
             raise TypeError()
-        return self.red == other.red and self.green == other.green and self.blue == other.blue
+
+        return self.color == other.color
 
     def __add__(self, other):
         return SaberCrystal(
-            color=[min(255, x + y) for x, y in zip(self.color, other.color if type(other) is SaberCrystal else other)])
+            [min(255, x + y) for x, y in zip(self.color, other.color if isinstance(other, SaberCrystal) else other)])
 
     def __sub__(self, other):
         return SaberCrystal(
-            color=[max(0, x - y) for x, y in zip(self.color, other.color if type(other) is SaberCrystal else other)])
+            [max(0, x - y) for x, y in zip(self.color, other.color if isinstance(other, SaberCrystal) else other)])
+
+    # def __contains__(self, item):
+    #     if isinstance(item, SaberCrystal):
+    #         item = item.color
+    #     for index, val in enumerate(item):
+    #         if val != 0 and self.color[index] != val:
+    #             return False
+    #     return True
 
     def __contains__(self, item):
-        if type(item) is SaberCrystal:
-            item = item.color
-        for index, val in enumerate(item):
-            if val != 0 and self.color[index] != val:
-                return False
-        return True
+        """
+        Returns true if all non-0 fields in the other item
+        match with this crystal's corresponding fields
+
+        Would probably actually go with the above implementation for better readability,
+        but I wanted to see if I could make it a one-liner :)
+        """
+
+        return all(val == 0 or self.color[index] == val for index, val in
+                   enumerate(item.color if isinstance(item, SaberCrystal) else item))
