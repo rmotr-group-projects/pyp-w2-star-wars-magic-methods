da0a0a91a420d65582bfcbde6dc76833c907e376
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-11-29 14:16:39 -0500

    alias for compatibility with python 2, answers to questions

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -2,15 +2,15 @@ class People(object):
 
     def __init__(self, name, dark_side=False):
         self.name = name
-        self.ddark_side = dark_side
+        self._dark_side = dark_side
 
     @property
     def light_side(self):
-        return not self.ddark_side
+        return not self._dark_side
 
     @property
     def dark_side(self):
-        return self.ddark_side
+        return self._dark_side
 
     def __str__(self):
         '''string'''
@@ -28,7 +28,10 @@ class People(object):
 
         # 1???: How does method know that other is using the name attribute from the class?
         # How come both other and other.name works here?
+        # ANS: because you have implemented the __str__ magic method. When the instance of the class is used in a string, it will return self.name
         return "%s swings a lightsaber at %s." % (self.name, other)
+
+    __div__ = __truediv__
 
     def __mul__(self, other):
         '''thermal detonator'''
@@ -57,18 +60,20 @@ class People(object):
     def __neg__(self):
         '''turn to dark side'''
 
-        # 2???: How come this doesn't work if we initialized dark_side like: self.dark_side = dark_side instead of self.ddark_side = dark_side?
-        self.ddark_side = True
+        # 2???: How come this doesn't work if we initialized dark_side like: self.dark_side = dark_side instead of self._dark_side = dark_side?
+        # ANS: Because you have defined the a property (through @property) with the same name
+        # See als https://docs.python.org/3/library/functions.html#property
+        self._dark_side = True
 
     def __pos__(self):
         '''turn to light side'''
 
-        self.ddark_side = False
+        self._dark_side = False
 
     def __invert__(self):
         '''change sides'''
 
-        self.ddark_side = self.light_side
+        self._dark_side = self.light_side
 
     def __xor__(self, other):
         '''force chokes'''
