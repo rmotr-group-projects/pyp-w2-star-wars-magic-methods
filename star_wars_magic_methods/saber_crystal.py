17713c4acad1a289538c02faead445a24161b3f5
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-11-30 01:14:32 +0000

    Add methods to SaberCrystal class

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -3,12 +3,48 @@ class SaberCrystal(object):
         self.red = color[0]
         self.green = color[1]
         self.blue = color[2]
-    
+
     @property
     def color(self):
         return (self.red, self.green, self.blue)
-    
+
     @color.setter
     def color(self):
         self.color = color
-        
+
+    def __eq__(self, other):
+        if self.color == other.color:
+            return True
+        return False
+
+    def __add__(self, other):
+        other = self.normalize(other)
+        r = self.eight_bit_color(self.red + other.red)
+        g = self.eight_bit_color(self.green + other.green)
+        b = self.eight_bit_color(self.blue + other.blue)
+        return SaberCrystal(color=(r,g,b))
+
+    def __sub__(self, other):
+        other = self.normalize(other)
+        r = self.red - other.red
+        g = self.green - other.green
+        b = self.blue - other.blue
+        return SaberCrystal(color=(r,g,b))
+
+    def  __contains__(self, other):
+        other = self.normalize(other)
+        if self.red > 0 and other.red > 0 or \
+            self.green > 0 and other.green > 0 or \
+            self.blue > 0 and other.blue > 0:
+            return True
+        return False
+
+    def normalize(self, other):
+        if isinstance(other, tuple):
+            return SaberCrystal(color=other)
+        return other
+
+    def eight_bit_color(self, value):
+        if value > 255:
+            value = 255
+        return value
