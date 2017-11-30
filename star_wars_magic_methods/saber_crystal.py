ed48db16f394602528c38de025189ee2acf26472
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-11-30 00:05:35 +0000

    Add attributes and color property to Saber class

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,14 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self.red = color[0]
+        self.green = color[1]
+        self.blue = color[2]
+    
+    @property
+    def color(self):
+        return (self.red, self.green, self.blue)
+    
+    @color.setter
+    def color(self):
+        self.color = color
+        
